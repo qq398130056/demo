698f5fa0728ca51c1bd1c2109d5e15dce21269c5
qq398130056 <398130056@qq.com>
2018-07-20 16:14:40 +0800

    ver2

--- src/test/java/com/iotek/SpringbootApplicationTests.java
+++ src/test/java/com/iotek/SpringbootApplicationTests.java
@@ -2,17 +2,28 @@ package com.iotek;
 
 import com.iotek.dao.StudentMapper;
 import com.iotek.entity.Student;
+import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cglib.core.KeyFactory;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootApplicationTests {
     @Autowired
 	StudentMapper studentMapper;
+	@Autowired
+	DataSource dataSource;
+
+	@Test
+	public void data(){
+		System.out.println(dataSource.getClass());
+	}
 	@Test
 	public void contextLoads() {
 		Student s = new Student();
